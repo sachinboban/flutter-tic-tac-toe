2b803f47bba1109db406ef7fe1ad2815504e8279
Sachin Boban <sachinbobank@gmail.com>
2020-04-07 11:10:04 -0600

    Add interaction to square.
    Modify Square to StatefulWidget to
    handle interactions.
    
    Signed-off-by: Sachin Boban <sachinbobank@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,37 @@ void main() {
   runApp(TicTacToe());
 }
 
-class Square extends StatelessWidget {
+class Square extends StatefulWidget {
   final int id;
-
   Square({this.id});
 
   @override
+  _SquareState createState() => _SquareState();
+}
+
+class _SquareState extends State<Square> {
+  bool _enabled = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListTile(
-        title: Text('Square $id'),
-      ),
-    );
+    if (_enabled) {
+      return Center(
+        child: ListTile(
+          title: Text('${widget.id}'),
+        ),
+      );
+    } else {
+      return Center(
+        child: ListTile(
+          title: Text(''),
+          onTap: () {
+            setState(() {
+              _enabled = true;
+            });
+          },
+        ),
+      );
+    }
   }
 }
 
